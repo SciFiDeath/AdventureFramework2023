160261271165e25be92831654ec1699f9e5217c0
Jona Kreis <jonkreis@ksr.ch>
2023-10-24 14:38:37 +0200

    all my fellas

--- Framework/Shared/Rooms/HM305/HM305Base.cs
+++ Framework/Shared/Rooms/HM305/HM305Base.cs
@@ -3,5 +3,6 @@ namespace Room;
 public class HM305Base : RoomBase
 {
 	// Custom functionality for HM305
-	protected string Points { get; set; } = "0,0 90%,0 100%,90% 20%, 100%";
+	protected string Points { get; set; } = "1319,176 1319,339 1081,336 1081,208";
 }
+
--- Template/Shared/Rooms/HM305/HM305.razor.cs
+++ Template/Shared/Rooms/HM305/HM305.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using System.Threading.Tasks;
 
 namespace Rooms;
 
