be2e61406a7fe2f053e2e5bd04c97886a661c13d
laherzig <laherzig@ksr.ch>
2023-12-05 14:25:20 +0100

    resolved annoying null dereference warning by telling compiler it shouldnt worry so much

--- Framework/Shared/JsonClasses.cs
+++ Framework/Shared/JsonClasses.cs
@@ -2,13 +2,13 @@ namespace Framework.Slides.JsonClasses;
 
 public class JsonSlide
 {
-	public string? Image { get; set; }
+	public string Image { get; set; } = null!;
 	public Dictionary<string, JsonButton> Buttons { get; set; } = null!;
 }
 
 public class JsonButton
 {
-	public string? Points { get; set; }
+	public string Points { get; set; } = null!;
 	public string? Image { get; set; }
 	public List<List<string>>? Actions { get; set; }
 }
