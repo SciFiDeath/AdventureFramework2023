eefe6b26fb6671b1f4a08379e4d8459b238e6186
Amos Forrer <amos.forrer@gmail.com>
2024-03-05 16:22:22 +0100

    correct files

---
+++ Framework/Minigames/Armdrügge.cs
@@ -0,0 +1,6 @@
+namespace Framework.Minigames.MinigameDefClasses;
+
+public class AmosGiannicegame : MinigameDefBase
+{
+    public override string BackgroundImage {get; set;} = "images/HM3_hallwayN.jpg";
+}
