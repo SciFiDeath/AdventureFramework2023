63e12362ca8ee77a42061b3bf61489a6eda43298
enggian <giaengel@ksr.ch>
2024-03-06 11:23:41 +0100

    random quadarte werden gespawnt

--- Framework/Minigames/Armdrügge.cs
+++ Framework/Minigames/Armdrügge.cs
@@ -3,20 +3,23 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Framework.Minigames.MinigameDefClasses;
 
+
 public class MyMinigame6 : MinigameDefBase
 {
+
+
     public override string BackgroundImage { get; set; } = "/images/HM3_hallwayN.jpg";
 
     [Element]
     public Rectangle Rect { get; set; }
 
 
-
+    public GameObjectContainer<Rectangle> Rects {get; set;} = new();
     public MyMinigame6()
     {
         Rect = new()
         {
-            Id = "rect1",
+            Id = "rect",
             X = 100,
             Y = 100,
             Width = 100,
@@ -26,30 +29,33 @@ public class MyMinigame6 : MinigameDefBase
 
         
         };
+            Rects.Add(Rect);
 
 
  Update();
     }
 
-public void hello(EventArgs args)
-{
-    Console.WriteLine("Hello");
-}
+
  public void spawnRect(EventArgs args)
     {
-        for (int i = 100; i < 1900; i+= 100)
-        {
+
+    Random rnd = new Random();
+
+    int randomx = rnd.Next(1,1900);
+    int randomy = rnd.Next(1,1080);
+        
             var rect = new Rectangle()
             {
-                X = i,
-                Y = 100,
+                X = randomx,
+                Y = randomy,
                 Width = 50,
                 Height= 50,
                 Fill = "red",
                 OnClick = tot,
             };
             AddElement(rect);
-        }
+            Rects.Add(rect);
+        
         Update();
     }
    public void tot(EventArgs args)
