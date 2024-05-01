bea9de1e4c43f5ae3fa56692fd7318dac0adbcae
laherzig <laherzig@ksr.ch>
2024-05-01 21:23:11 +0200

    Add tasks that runs after build to write names of json files in folder to file

---
+++ BuildTasks/WriteFilePaths.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace BuildTasks;
+
+public class WriteFilePaths : Microsoft.Build.Utilities.Task
+{
+	[Required]
+	public string DirectoryPath { get; set; } = null!;
+
+	[Required]
+	public string OutputFilePath { get; set; } = null!;
+
+	public override bool Execute()
+	{
+		try
+		{
+			var files = Directory.GetFiles(DirectoryPath, "*.json", SearchOption.TopDirectoryOnly);
+			var fileNames = files.Select(Path.GetFileName).ToArray();
+
+			// suppress warning cause andnoying
+			File.WriteAllLines(OutputFilePath, fileNames!);
+			return true;
+		}
+		catch (Exception ex)
+		{
+			Log.LogErrorFromException(ex);
+			return false;
+		}
+	}
+}
