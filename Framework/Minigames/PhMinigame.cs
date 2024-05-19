38b7713067c3ac201bd43ff309729079daa3a21c
Zeynep Zülal <119702784+zeynepzulal@users.noreply.github.com>
2024-05-19 22:27:18 +0200

    m

--- Framework/Minigames/PhMinigame.cs
+++ Framework/Minigames/PhMinigame.cs
@@ -2,56 +2,139 @@
 
 public class PhMinigame : MinigameDefBase
 {
-    public override string BackgroundImage { get; set; } = "images/PhMinigame/stromkreis.jpg";
+    public override string BackgroundImage { get; set; } = "images/PhMinigame/platform2.jpg";
 
-    public SVGImage RectRed { get; set; }
-
-    public Rectangle RectYellow { get; set; }
-
+    static List<Rectangle> ClickedWhiteBoards = new List<Rectangle>();
+    public SVGImage YellowMaterial { get; set; }
+    public SVGImage BlueMaterial { get; set; }
+    public SVGImage PinkMaterial { get; set; }
+    public SVGImage GreenMaterial { get; set; }
+    public Rectangle WhiteBoard { get; set; }
     public SVGImage SelectedElement { get; set; }
+    public SVGImage SelectedWhiteBoard { get; set; }
 
     public PhMinigame()
     {
-        RectRed = new()
+        YellowMaterial = new()
         {
-            X = 1350,
-            Y = 300,
-            Width = 100,
-            Height = 100,
-            Image = "images/PhMinigame/1.jpg",
-            OnClick = ClickRed
-            //OnClick = (args) => { Console.WriteLine("Hello World!"); }
+            X = 1500,
+            Y = 200,
+            Width = 140,
+            Height = 135,
+            Image = "images/PhMinigame/m1.jpg",
+            OnClick = ClickYellow
         };
+        AddElement(YellowMaterial);
 
-        RectYellow = new()
+        BlueMaterial = new()
         {
-            X = 700,
-            Y = 500,
-            Width = 100,
-            Height = 100,
-            Fill = "transparent",
-            ZIndex = -1,
-            OnClick = MoveRed
+            X = 1500,
+            Y = 400,
+            Width = 140,
+            Height = 135,
+            Image = "images/PhMinigame/m4.jpg",
+            OnClick = ClickBlue
         };
+        AddElement(BlueMaterial);
 
+        PinkMaterial = new()
+        {
+            X = 1500,
+            Y = 600,
+            Width = 140,
+            Height = 135,
+            Image = "images/PhMinigame/m2.jpg",
+            OnClick = ClickPink
+        };
+        AddElement(PinkMaterial);
 
-        AddElement(RectRed);
-        AddElement(RectYellow);
+        GreenMaterial = new()
+        {
+            X = 1500,
+            Y = 800,
+            Width = 140,
+            Height = 135,
+            Image = "images/PhMinigame/m3.jpg",
+            OnClick = ClickGreen
+        };
+        AddElement(GreenMaterial);
+
+        /*
+        WhiteBoard = new Rectangle()
+        {
+            X = 450,
+            Y = 50,
+            Width = 140,
+            Height = 135,
+            Fill = "blue",
+            ZIndex = -2,
+            OnClick = MoveMaterial
+        };
+        AddElement(WhiteBoard);
+       */
+        
+       for (int i = 0; i < (140* 6); i += 140)
+        {
+            for (int j = 0; j < (135* 8); j += 135)
+            {
+                WhiteBoard = new Rectangle ()
+                {
+                    X = i + 450,
+                    Y = j + 50,
+                    Width = 140,
+                    Height = 135,
+                    Fill = "transparent",
+                    ZIndex = -2,
+                    OnClick = ClickWhiteBoard
+                };
+                AddElement(WhiteBoard);
+                
+            }
+
+        }
+
     }
 
-
-    public void ClickRed(EventArgs args)
+    
+    public void ClickYellow(EventArgs args)
     {
         Console.WriteLine("Hello World!");
-        SelectedElement = RectRed;
+        SelectedElement = YellowMaterial;
     }
-
-    public void MoveRed(EventArgs args)
+    public void ClickBlue(EventArgs args)
     {
-        SelectedElement.X = RectYellow.X;
-        SelectedElement.Y = RectYellow.Y;
-  
-        Update();
+        Console.WriteLine("Hello World!");
+        SelectedElement = BlueMaterial;
     }
+    public void ClickPink(EventArgs args)
+    {
+        Console.WriteLine("Hello World!");
+        SelectedElement = PinkMaterial;
+    }
+    public void ClickGreen(EventArgs args)
+    {
+        Console.WriteLine("Hello World!");
+        SelectedElement = GreenMaterial;
+    }
+    /*
+    public void MoveMaterial()
+    {
+        SelectedElement.X = SelectedWhiteBoard.X;
+        SelectedElement.Y = SelectedWhiteBoard.Y;
+        //WhiteBoard.Image = SelectedElement.Image;
+        Update();
+    } 
+    */
+    public void ClickWhiteBoard()
+    {
+        Console.WriteLine("white board clicked");
+        ClickedWhiteBoards.Add(SelectedWhiteBoard);
+        //ClickedWhiteBoards.Last().X = SelectedWhiteBoard.X;
+        //ClickedWhiteBoards.Last().Y = SelectedWhiteBoard.Y;
+        SelectedElement.X = SelectedWhiteBoard.X;
+        SelectedElement.Y = SelectedWhiteBoard.Y;
+        Update();
+    }  
+  
 
 }
