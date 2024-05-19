b571c8f4487039fd1c44e700a8e5f1d940b0241e
enggian <giaengel@ksr.ch>
2024-05-19 04:15:22 +0200

    Timer works, but changing the image when the opponent was faster is broken.

--- Framework/Minigames/Armdrügge.cs
+++ Framework/Minigames/Armdrügge.cs
@@ -5,13 +5,9 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace Framework.Minigames.MinigameDefClasses;
 
-
 public class MyMinigame6 : MinigameDefBase
 {
-
-
     public override string BackgroundImage { get; set; } = "/images/Armdrücken test.jpg";
-
 
     public MyMinigame6()
     {
@@ -19,91 +15,95 @@ public class MyMinigame6 : MinigameDefBase
         int clickcount = 0; // Wie oft man auf den Kreis gedrückt hat (für die Farben zustädnig)
         int Ycord = 758; //Unterster Startpunkt
         int enemyYcord = 758;
-
-
-
-        AddElement(
-                  new Rectangle()
-                  {
-                      // Progressbar links (Spieler)
-                      X = 80,
-                      Y = 150,
-                      Width = 150,
-                      Height = 700,
-                      Fill = "transparent",
-                      Stroke = "black",
-                      StrokeWidth = 40,
-                  }
-              );
+        int score = 0;
 
         AddElement(
-                  new Rectangle()
-                  {
-                      //Progressbar rechts (Gegner)
-                      X = 1375,
-                      Y = 150,
-                      Width = 150,
-                      Height = 700,
-                      Fill = "transparent",
-                      Stroke = "black",
-                      StrokeWidth = 40,
-                  }
-              );
+              new Rectangle()
+              {
+                  // Progressbar links (Spieler)
+                  X = 80,
+                  Y = 150,
+                  Width = 150,
+                  Height = 700,
+                  Fill = "transparent",
+                  Stroke = "black",
+                  StrokeWidth = 40,
+              }
+          );
 
         AddElement(
-                  new Circle()
+              new Rectangle()
+              {
+                  //Progressbar rechts (Gegner)
+                  X = 1375,
+                  Y = 150,
+                  Width = 150,
+                  Height = 700,
+                  Fill = "transparent",
+                  Stroke = "black",
+                  StrokeWidth = 40,
+              }
+          );
+
+        AddElement(
+              new Circle()
+              {
+                  //Hand zum klicken
+                  R = 100,
+                  CX = 550,
+                  CY = 500,
+                  Fill = "orange",
+                  Stroke = "red",
+                  StrokeWidth = 40,
+                  OnClick = (args) =>
                   {
-                      //Hand zum klicken
-                      R = 100,
-                      CX = 550,
-                      CY = 500,
-                      Fill = "orange",
-                      Stroke = "red",
-                      StrokeWidth = 40,
-                      OnClick = (args) => { progressClick(Ycord, clickcount); Ycord = Ycord - 107; clickcount++; imageswap(ref clickcount, ref enemycounter, ref Ycord, ref enemyYcord); }, //OnClick wird Funktion ausgeführt, die die Füllung macht und Ycord wird angepasst, damit es hoch geht
-                  }
-              );
+                      progressClick(Ycord, clickcount);
+                      Ycord = Ycord - 107;
+                      clickcount++;
+                      imageswap(ref clickcount, ref enemycounter, ref Ycord, ref enemyYcord, ref score);
+                  }, //OnClick wird Funktion ausgeführt, die die Füllung macht und Ycord wird angepasst, damit es hoch geht
+              }
+          );
         Update();
 
-        while (enemycounter < 6)
+        EnemyState enemyState = new EnemyState
         {
-            // Solange enemyounter unter 6 ist (6, weil wir 6 Farben haben von Weiss bis und mit Rot)
-            enemyclick(enemyYcord, enemycounter);
-            enemycounter++;
-            enemyYcord = enemyYcord - 107;
-            Update();
-        }
-        Update();
+            Ycord = enemyYcord,
+            EnemyCounter = enemycounter,
+            EnemyYcord = enemyYcord
+        };
 
+        _ = StartEnemyClick(enemyState);
+        Update();
     }
 
-    public void imageswap(ref int yourscore, ref int enemyscore, ref int Ycord, ref int enemyYcord)
+    public void imageswap(ref int yourscore, ref int enemyscore, ref int Ycord, ref int enemyYcord, ref int gamescore)
     {
-        int score = 0;
+
 
         if (yourscore == 6)
         {
-            score++;
+            gamescore++;
             yourscore = 0;
             Ycord = 758;
         }
-        else if (enemyscore == 6)
+        else if (enemyscore == 5)
         {
-            score--;
+            gamescore--;
             enemyscore = 0;
             enemyYcord = 758;
         }
-        if (score == 0)
+        if (gamescore == 0)
         {
             BackgroundImage = "/images/Armdrücken test.jpg";
             Update();
         }
-        else if (score == 1)
+        else if (gamescore == 1)
         {
             BackgroundImage = "/images/calculator.png";
             Update();
         }
-        else if (score == -1)
+        else if (gamescore == -1)
         {
             BackgroundImage = "/images/HM3_hallwayN.jpg";
             Update();
@@ -111,22 +111,31 @@ public class MyMinigame6 : MinigameDefBase
         Update();
     }
 
-    public void enemyclick(int Ycord, int enemycounter) //Funktion, die für das Auffüllen der Gegnerischen Progressbar zuständig ist
+    public async Task StartEnemyClick(EnemyState enemyState)
     {
-        List<string> colors = new List<string> { "LightGoldenrodYellow", "yellow", "Gold", "orange", "DarkOrange", "red" };
-        AddElement(
-               new Rectangle()
-               {
-                   X = 1445,
-                   Y = Ycord,
-                   Width = 10,
-                   Height = 21,
-                   Fill = "transparent",
-                   Stroke = colors[enemycounter],
-                   StrokeWidth = 100
-               }
-           );
-        Update();
+        using PeriodicTimer timer = new(TimeSpan.FromMilliseconds(1000));
+
+        while (enemyState.EnemyCounter < 6 && await timer.WaitForNextTickAsync())
+        {
+            List<string> colors = new List<string> { "LightGoldenrodYellow", "yellow", "Gold", "orange", "DarkOrange", "red" };
+            AddElement(
+                   new Rectangle()
+                   {
+                       X = 1445,
+                       Y = enemyState.EnemyYcord,
+                       Width = 10,
+                       Height = 21,
+                       Fill = "transparent",
+                       Stroke = colors[enemyState.EnemyCounter],
+                       StrokeWidth = 100
+                   }
+               );
+
+            enemyState.EnemyCounter++;
+            enemyState.EnemyYcord = enemyState.EnemyYcord - 107;
+
+            Update();
+        }
     }
 
     public void progressClick(int Ycord, int counter)
@@ -151,6 +160,10 @@ public class MyMinigame6 : MinigameDefBase
         Update();
     }
 
+    public class EnemyState
+    {
+        public int Ycord { get; set; }
+        public int EnemyCounter { get; set; }
+        public int EnemyYcord { get; set; }
+    }
 }
-
-
