67289595c98c1deef79d35e88f5cc2ce5e38d6d6
luzendel <luzendel@ksr.ch>
2024-05-28 15:49:45 +0200

    New Version 28/05/2024 15:49

--- Framework/Minigames/BiologyMinigame.cs
+++ Framework/Minigames/BiologyMinigame.cs
@@ -7,18 +7,8 @@ public class BiologyMinigame : MinigameDefBase
 {
     int score = 0;
     int lives = 3;
-    int seconds = 60;
-    int QuestionNumber = 1;
-    public override async Task GameLoop(CancellationToken token)
-    {
-        while (seconds != 0)
-        {
-            await Task.Delay(1000);
-            seconds = seconds - 1;
-            token.ThrowIfCancellationRequested();
-        }
-    }
-    public override string BackgroundImage {get; set;} = "/images/Cooles_Bild.jpg";
+    int questionNumber = 1;
+    public override string BackgroundImage {get; set;} = "/images/Cooles_Bild.JPG";
     [Element]
     public Rectangle Rect {get; set;}
     [Element]
@@ -30,33 +20,30 @@ public class BiologyMinigame : MinigameDefBase
     [Element]
     public Text LifeText {get; set;}
     [Element]
-    public Text TimeText {get; set;}
+    public Text QuestionOne {get; set;}
     public GameObjectContainer<Rectangle> Rects {get; set;} = new();
     public GameObjectContainer<Rectangle> Rects2 {get; set;} = new();
     public GameObjectContainer<Text> TheTexts {get; set;} = new();
     public GameObjectContainer<Text> TheTexts2 {get; set;} = new();
     public GameObjectContainer<Text> LifeTexts {get; set;} = new();
-    public GameObjectContainer<Text> TimeTexts {get; set;} = new();
-
     public BiologyMinigame()
     {
-        TimeText = new()
+        QuestionOne = new()
         {
-            InnerText = "Time: " + seconds,
-            X = 1500,
+            InnerText = "Ist x ein y?",
+            X = 1000,
             Y = 100,
-            FontSize = 75,
+            FontSize = 100,
             FontFamily = "sans-serif",
-            Fill = "red", 
+            Fill = "green"
         };
-        TimeTexts.Add(TimeText);
 
         LifeText = new()
         {
             InnerText = "Lives: " + lives,
             X = 100,
             Y = 100,
-            FontSize = "75",
+            FontSize = 75,
             FontFamily = "sans-serif",
             Fill = "red" 
         };
@@ -67,7 +54,7 @@ public class BiologyMinigame : MinigameDefBase
             InnerText = "Richtig",
             X = 300,
             Y = 1000,
-            FontSize = "100",
+            FontSize = 100,
             FontFamily = "sans-serif",
             Fill = "white" 
         };
@@ -78,7 +65,7 @@ public class BiologyMinigame : MinigameDefBase
             InnerText = "Falsch",
             X = 1100,
             Y = 1000,
-            FontSize = "100",
+            FontSize = 100,
             FontFamily = "sans-serif",
             Fill = "white"
         };
