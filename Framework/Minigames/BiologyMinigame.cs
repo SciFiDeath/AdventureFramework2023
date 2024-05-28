dc6947fdcc44ad729df161f71398a266c97a0151
luzendel <luzendel@ksr.ch>
2024-05-28 16:11:11 +0200

    New Version 28/05/2024 16:11

--- Framework/Minigames/BiologyMinigame.cs
+++ Framework/Minigames/BiologyMinigame.cs
@@ -5,9 +5,34 @@ namespace Framework.Minigames.MinigameDefClasses;
 
 public class BiologyMinigame : MinigameDefBase
 {
-    int score = 0;
+    int currentScore = 0;
     int lives = 3;
     int questionNumber = 1;
+    bool answer = true;
+    public bool RichtigKnopf()
+    {
+        answer = true;
+        if (questionNumber % 2 == 1)
+        {
+            lives--;
+            currentScore--;
+        }
+        currentScore++;
+        questionNumber++;
+        return answer;
+    }
+    public bool FalschKnopf()
+    {
+        answer = false;
+        if (questionNumber % 2 == 0)
+        {
+            lives--;
+            currentScore--;
+        }
+        currentScore++;
+        questionNumber++;
+        return false;
+    }
     public override string BackgroundImage {get; set;} = "/images/Cooles_Bild.JPG";
     [Element]
     public Rectangle Rect {get; set;}
@@ -21,13 +46,26 @@ public class BiologyMinigame : MinigameDefBase
     public Text LifeText {get; set;}
     [Element]
     public Text QuestionOne {get; set;}
+    [Element]
+    public Text ScoreText {get; set;}
     public GameObjectContainer<Rectangle> Rects {get; set;} = new();
     public GameObjectContainer<Rectangle> Rects2 {get; set;} = new();
     public GameObjectContainer<Text> TheTexts {get; set;} = new();
     public GameObjectContainer<Text> TheTexts2 {get; set;} = new();
     public GameObjectContainer<Text> LifeTexts {get; set;} = new();
+    public GameObjectContainer<Text> ScoreTexts {get; set;} = new();
     public BiologyMinigame()
     {
+        ScoreText = new()
+        {
+            InnerText = "Score: " + currentScore + "/5",
+            X = 100,
+            Y = 200,
+            FontSize = 75,
+            FontFamily = "sans-serif",
+            Fill = "red"
+        };
+        ScoreTexts.Add(ScoreText);
         QuestionOne = new()
         {
             InnerText = "Ist x ein y?",
