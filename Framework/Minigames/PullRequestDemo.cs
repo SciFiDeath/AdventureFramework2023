6376155a5d95d5b1dbc8d9e98b7f4dbd90e326aa
laherzig <laherzig@ksr.ch>
2024-05-28 14:28:52 +0200

    add minigame

---
+++ Framework/Minigames/PullRequestDemo.cs
@@ -0,0 +1,35 @@
+using Framework.Minigames;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace Framework.Minigames.MinigameDefClasses;
+
+public class Demo : MinigameDefBase
+{
+	public override string BackgroundImage { get; set; } = "images/HM305_blackboard.jpg";
+
+	public void Test(EventArgs e)
+	{
+		Rect.X += 20;
+		Update();
+		if (Rect.X > 300)
+		{
+			Finish(null, "HM305Beamer");
+		}
+	}
+
+	[Element] public Rectangle Rect { get; set; }
+
+	public Demo()
+	{
+		Rect = new()
+		{
+			X = 100,
+			Y = 100,
+			Width = 100,
+			Height = 100,
+			ZIndex = 1,
+			Fill = "red",
+			OnClick = Test
+		};
+	}
+}
