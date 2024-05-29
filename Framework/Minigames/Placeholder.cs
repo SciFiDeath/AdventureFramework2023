4e0c26ccf34ea2ad4d44c8360d33f5164fbc5ca3
rebajram <bajramoskarejda@gmail.com>
2024-05-29 23:03:08 +0200

    every error with name tag i'll cry bye

--- Framework/Minigames/Placeholder.cs
+++ Framework/Minigames/Placeholder.cs
@@ -55,7 +55,7 @@ public class MinigameTut : MinigameDefBase
         AddElement(newRect);
         Rects.Kill();
         bool found = false;
-        Error0 = new()
+        Error0 = new() //Box
         {
             X = 1200,
             Y = 10,
@@ -68,7 +68,7 @@ public class MinigameTut : MinigameDefBase
         Errors.Add(Error0);
         AddElement(Error0);
         Update();
-        Error1 = new()
+        Error1 = new() //blue zylinder
         {
             X = 1000,
             Y = 350,
@@ -80,6 +80,110 @@ public class MinigameTut : MinigameDefBase
         };
         Errors.Add(Error1);
         AddElement(Error1);
+        Update();
+        Error2 = new() //purple strip
+        {
+            X = 1450,
+            Y = 360,
+            Width = 110,
+            Height = 200,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error2)
+
+        };
+        Errors.Add(Error2);
+        AddElement(Error2);
+        Update();
+        Error3 = new()//spoons
+        {
+            X = 1380,
+            Y = 850,
+            Width = 150,
+            Height = 250,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error3)
+
+        };
+        Errors.Add(Error3);
+        AddElement(Error3);
+        Update();
+        Error4 = new() // black lid
+        {
+            X = 1260,
+            Y = 510,
+            Width = 120,
+            Height = 200,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error4)
+
+        };
+        Errors.Add(Error4);
+        AddElement(Error4);
+        Update();
+        Error5 = new()//B12 Box upside down
+        {
+            X = 1180,
+            Y = 620,
+            Width = 55,
+            Height = 100,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error5)
+
+        };
+        Errors.Add(Error5);
+        AddElement(Error5);
+        Update();
+        Error6 = new() //blue lid upside down
+        {
+            X = 792,
+            Y = 390,
+            Width = 100,
+            Height = 180,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error6)
+
+        };
+        Errors.Add(Error6);
+        AddElement(Error6);
+        Update();
+        Error7 = new() //türkis lid moved
+        {
+            X = 750,
+            Y = 600,
+            Width = 100,
+            Height = 140,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error7)
+
+        };
+        Errors.Add(Error7);
+        AddElement(Error7);
+        Update();
+        Error8 = new() //cutting board
+        {
+            X = 750,
+            Y = 770,
+            Width = 100,
+            Height = 300,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error8)
+
+        };
+        Errors.Add(Error8);
+        AddElement(Error8);
+        Update();
+        Error9 = new()//missing 123
+        {
+            X = 560,
+            Y = 630,
+            Width = 75,
+            Height = 100,
+            Fill = "transparent",
+            OnClick = (args) => ChangeColor(args, Error9)
+
+        };
+        Errors.Add(Error9);
+        AddElement(Error9);
         Update();
     }
     public void originalpage(EventArgs e)
@@ -123,7 +227,7 @@ public class MinigameTut : MinigameDefBase
     }
     public void endgame() 
     {
-        if (errorsspotted == 2) 
+        if (errorsspotted == 10) 
         {
             Finish(null, "test");
         }
