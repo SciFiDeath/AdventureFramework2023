f67bb6803deb9003bd9fa84622e59be58e5189d6
habooom <habooom88@gmail.com>
2024-05-31 23:26:11 +0200

    fertig

--- Framework/Minigames/Placeholder.cs
+++
@@ -1,5 +0,0 @@
-namespace Framework.Minigames.MinigameDefClasses;
-
-public class MinigameTut : MinigameDefBase{
-    public override string BackgroundImage {get; set;} = "images/HM3 hallwayN.jpg";
-}
